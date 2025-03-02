846359a6357082ee624947a2b68119ed00c02a55
Elias Castro <elias.castro@legalmanager.com.br>
2025-03-01 22:14:11 -0300

    refactor: Alteração do Service para chamar as classes do Strategy.

--- src/main/java/br/com/castro/strategynotification/service/NotificationService.java
+++ src/main/java/br/com/castro/strategynotification/service/NotificationService.java
@@ -1,15 +1,32 @@
 package br.com.castro.strategynotification.service;
 
-import lombok.extern.slf4j.Slf4j;
+import br.com.castro.strategynotification.service.strategy.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import java.util.Map;
+
 @Service
 public class NotificationService {
 
+    private final Map<String, NotificationStrategy> strategyMap = Map.of(
+            "discord", new DiscordNotificationStrategy(),
+            "instagram", new InstagramNotificationStrategy(),
+            "twitter", new TwitterNotificationStrategy(),
+            "email", new EmailNotificationStrategy(),
+            "whatsapp", new WhatsappNotificationStrategy()
+    );
+
     private static final Logger log = LoggerFactory.getLogger(NotificationService.class);
 
+    public void notify(String channel, String destination, String message) {
+        strategyMap.get(channel).sendNotification(destination, message);
+    }
+
+
+    // Método deixado para demonstrar como era feito antes
+    /*
     public void notify(String channel, String destination, String message) {
 
         if(channel.equalsIgnoreCase("discord")) {
@@ -30,6 +47,6 @@ public class NotificationService {
         }else {
             throw new IllegalArgumentException("Channel not supported");
         }
-
     }
+    */
 }
